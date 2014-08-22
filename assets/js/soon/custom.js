416210fda7d08040fcf64a50a3314ce0332bdd9e
Dirk Dittmar <dirk.dittmar@web.de>
2014-08-23 00:36:02 +0200

    - aufgeräumt - neue Bilder - Texte angepasst

--- assets/js/soon/custom.js
+++ assets/js/soon/custom.js
@@ -24,7 +24,7 @@ COMMING SOON PAGE
             hours.html('<h1>0</h1><p>Hour</p>');
             minutes.html('<h1>0</h1><p>Minute</p>');
             seconds.html('<h1>0</h1><p>Second</p>');
-            message.html('Dirk ist jetzt bei Monday Consulting!');
+            message.html('<b>Dirk</b> ist jetzt bei <b>Monday Consulting</b>...');
         }
         else{
             var s = -now.getTimezoneOffset()*60 + (launch.getTime() - now.getTime())/1000;
